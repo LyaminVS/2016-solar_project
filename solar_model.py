0b4383a4a9b8218f2906f0431cc51a9cb3f3ffb8
kozhurkinva <kozhurkin.va@do-student.ru>
2021-11-05 17:04:04 +0300

    формулы сил в моделировании

--- solar_model.py
+++ solar_model.py
@@ -18,9 +18,11 @@ def calculate_force(body, space_objects):
     for obj in space_objects:
         if body == obj:
             continue  # тело не действует гравитационной силой на само себя!
-        r = ((body.x - obj.x)**2 + (body.y - obj.y)**2)**0.5
-        body.Fx += 1  # FIXME: нужно вывести формулу...
-        body.Fy += 2  # FIXME: нужно вывести формулу...
+        dx, dy = body.x - obj.x, body.y - obj.y
+        r = (dx**2 + dy**2)**0.5
+        F = gravitational_constant * body.m * obj.m / r ** 2
+        body.Fx += F * dx / r
+        body.Fy += F * dy / r
 
 
 def move_space_object(body, dt):
