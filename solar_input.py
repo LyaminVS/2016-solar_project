620c2b891b3bbf82842aa7ab087ec6bd6d712726
kozhurkinva <kozhurkin.va@do-student.ru>
2021-11-05 16:46:03 +0300

    продолжил править инпут

--- solar_input.py
+++ solar_input.py
@@ -47,8 +47,8 @@ def parse_star_parameters(line, star):
     **line** — строка с описание звезды.
     **star** — объект звезды.
     """
+    star.Star, star.R, star.color, star.m, star.x, star.y, star.Vx, star.Vy = line.split()
 
-    pass  # FIXME: not done yet
 
 def parse_planet_parameters(line, planet):
     """Считывает данные о планете из строки.
@@ -65,7 +65,7 @@ def parse_planet_parameters(line, planet):
     **line** — строка с описание планеты.
     **planet** — объект планеты.
     """
-    pass  # FIXME: not done yet...
+    planet.Star, planet.R, planet.color, planet.m, planet.x, planet.y, planet.Vx, planet.Vy = line.split()
 
 
 def write_space_objects_data_to_file(output_filename, space_objects):
@@ -83,6 +83,7 @@ def write_space_objects_data_to_file(output_filename, space_objects):
         for obj in space_objects:
             print(out_file, "%s %d %s %f" % ('1', 2, '3', 4.5))
             # FIXME: should store real values
+
 
 # FIXME: хорошо бы ещё сделать функцию, сохранающую статистику в заданный файл...
 
